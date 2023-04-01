4b5483da9ce283a5ab72edfd46d24a3a2248443e
pascal-prfst <pascal.purfuerst@googlemail.com>
2023-04-01 22:28:10 +0200

    styled todo-list component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TodoInterface } from '../_interface/todo-interface';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
-
+  todos: TodoInterface[] = [];
 }
